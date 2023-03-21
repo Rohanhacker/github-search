1ebc1b40ef5c69e9d34a447900cf765f0e48321b
rohanhacker <rohan.root@gmail.com>
2023-03-21 17:41:14 +0900

    init nextjs and added tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// `pages/_app.js`
+import "../styles/globals.css"
+import { AppProps } from "next/app"
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
